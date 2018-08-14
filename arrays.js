3134a6148544bf513d18318994aba6308b53e04c
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-14 13:05:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,4 @@ var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"]
 var addElementToBeginningOfArray = [1];
 const array = ["foo", ...addElementToBeginningOfArray]
 console.log(array)
+
