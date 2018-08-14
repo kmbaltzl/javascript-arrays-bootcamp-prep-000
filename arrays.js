6b02ed52f32d4c6da98a1a3dd50884d778248812
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-14 13:27:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,10 +5,7 @@ return [element,...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return destructivelyAddElementToBeginningOfArray.unshift("element")
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
