b83c114f7c6aef7f5e289c36ab1c8ad5b6afcede
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-14 13:47:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   return array
 }
 
-function removeElementFromBeginningOfArray(element, array) {
+function removeElementFromBeginningOfArray(array, element) {
   array.slice(1)
   return array
 }
